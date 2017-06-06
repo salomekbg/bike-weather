fad6b70aacf0c2e262fdc3298665956ab3a3aed7
SluvsP <salomekbg@gmail.com>
2017-06-06 16:12:47 -0400

    coerce dates for weather and citibike models

--- app/models/citibike.rb
+++ app/models/citibike.rb
@@ -4,6 +4,7 @@ class Citibike < ApplicationRecord
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       citibike_hash = row.to_h
+      citibike_hash["date"].insert(-3, "20")
       Citibike.create!(citibike_hash)
     end
   end
--- app/models/weather.rb
+++ app/models/weather.rb
@@ -4,6 +4,9 @@ class Weather < ApplicationRecord
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       weather_hash = row.to_h
+      new_date = weather_hash["date"][4..5] + "/" + weather_hash["date"][6..7] + "/" + weather_hash["date"][0..3]
+      date_array = new_date.split('/')
+      weather_hash["date"] = date_array.each {|e| e.sub!(/^0/, '')}.join('/')
       Weather.create!(weather_hash)
     end
   end
