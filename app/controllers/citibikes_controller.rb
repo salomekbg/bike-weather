0416abcc959659134e68c1442003f2d743d149fe
SluvsP <salomekbg@gmail.com>
2017-06-06 14:50:29 -0400

    set up adding citibike csv to database

--- app/controllers/citibikes_controller.rb
+++ app/controllers/citibikes_controller.rb
@@ -1,2 +1,8 @@
 class CitibikesController < ApplicationController
+
+  def import
+    Citibike.import(params[:citibike_file])
+    redirect_to root_path, notice: "Citibike information imported."
+  end
+  
 end
--- app/models/citibike.rb
+++ app/models/citibike.rb
@@ -1,2 +1,10 @@
 class Citibike < ApplicationRecord
+  require 'csv'
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      citibike_hash = row.to_h
+      Citibike.create!(citibike_hash)
+    end
+  end
 end
