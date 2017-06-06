2b46ca154dd56d9ebc2c7050a807955427a848ee
SluvsP <salomekbg@gmail.com>
2017-06-06 15:03:04 -0400

    update welcome, citibikes, weather views

--- app/controllers/citibikes_controller.rb
+++ app/controllers/citibikes_controller.rb
@@ -1,8 +1,12 @@
 class CitibikesController < ApplicationController
 
+  def index
+    @citibikes = Citibike.all
+  end
+
   def import
     Citibike.import(params[:citibike_file])
     redirect_to root_path, notice: "Citibike information imported."
   end
-  
+
 end
--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,8 +1,12 @@
 class WeathersController < ApplicationController
 
+  def index
+    @weathers = Weather.all
+  end
+
   def import
     Weather.import(params[:weather_file])
     redirect_to root_path, notice: "Weather information imported."
   end
-  
+
 end
