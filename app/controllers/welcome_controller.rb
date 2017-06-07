9a96e46e3909a47283b5de8f86ad4030252d16a7
SluvsP <salomekbg@gmail.com>
2017-06-07 17:56:55 -0400

    add chartkick - google charts to web app

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  require 'byebug'
 
   def index
     if params[:search]
@@ -7,6 +8,15 @@ class WelcomeController < ApplicationController
       @weathers = Weather.search(@date)
     end
     render 'index'
+  end
+
+  def show
+    @citibikes = Citibike.all.order('date')
+    @weathers = Weather.all.order('date').where(tmax: -50..150, date: "04/01/2017".."05/31/3017")
+    @min_temps = @weathers.map {|weather| [weather.date, weather.tmin]}
+    @max_temps = @weathers.map {|weather| [weather.date, weather.tmax]}
+    @trips = @citibikes.map {|citibike| [citibike.date, citibike.trips]}
+    @miles = @citibikes.map {|citibike| [citibike.date, citibike.miles]}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     end
   end
 
+  get '/welcome/show', to: 'welcome#show'
   root to: "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
