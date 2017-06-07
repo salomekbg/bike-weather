6a57ce2a6f0dfebf704809a3b8744cbefc64fd5b
SluvsP <salomekbg@gmail.com>
2017-06-07 09:57:46 -0400

    remove citibike.all and weather.all from welcome controller and add if statement to check for search and to find citibike and weather that matches

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,11 @@
 class WelcomeController < ApplicationController
+  require 'byebug'
 
   def index
-    @citibikes = Citibike.all
-    @weathers = Weather.all
+    if params[:search]
+      @citibikes = Citibike.search(params[:search])
+      @weathers = Weather.search(params[:search])
+    end
   end
 
 end
