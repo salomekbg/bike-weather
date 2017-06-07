f6a616d20e42b53806008a31afc9e947e896c62c
SluvsP <salomekbg@gmail.com>
2017-06-07 10:22:54 -0400

    coerce citibike data

--- app/models/citibike.rb
+++ app/models/citibike.rb
@@ -1,16 +1,15 @@
 class Citibike < ApplicationRecord
   require 'csv'
-  require 'byebug'
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       citibike_hash = row.to_h
-      citibike_date_array = citibike_hash["date"].split('/')
+      citibike_date_array = citibike_hash["Date"].split('/')
       citibike_date_array[0].length == 1 ? citibike_date_array[0].insert(0, "0") : nil
       citibike_date_array[1].length == 1 ? citibike_date_array[1].insert(0, "0") : nil
-      citibike_date_array[2].insert(0, "20")
-      citibike_hash["date"] = citibike_date_array.join('/')
-      Citibike.create!(citibike_hash)
+      citibike_date_array[2].length == 2 ? citibike_date_array[2].insert(0, "20") : nil
+      citibike_hash["Date"] = citibike_date_array.join('/')
+      Citibike.create(date: citibike_hash["Date"], trips: citibike_hash["Trips over the past 24-hours (midnight to 11:59pm)"], miles: citibike_hash["Miles traveled today (midnight to 11:59 pm)"])
     end
   end
 
